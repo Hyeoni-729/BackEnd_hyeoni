90697801b0ce80aaea166770d35e955c5b394967
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-04-25 16:50:23 +0900

    1일차 완료

--- 20250425/first.py
+++ 20250425/first.py
@@ -1,3 +1,43 @@
-print("안녕")
-print("내 이름은 심희현")
+# print("안녕")
+# print("내 이름은 심희현")
 
+print("😀😀😀😀😀")
+print("😀      😀")
+print("😀      😀")
+
+# 변수
+# 변수이름 = 값
+# 변수이름은 의미있어야 한다.
+
+name = "된장찌개"
+name = "콩나물비빔밥"
+
+# 위에서부터 차례대로 출력
+name = "된장찌개"
+print(name)
+name = "콩나물비빔밥"
+print(name)
+
+# 두 개의 변수는 다른 변수다
+'''
+test = 10
+Test = 20
+'''
+
+temp = "a"
+test = "b"
+num = "10"
+name = "lubi"
+year = "2025"
+print(temp, test, num, name, year)
+
+#숫자는 따옴표 붙이지 않는다
+a = 10
+b = 10
+# c = 20
+# print(a, " + ", b, " = ", c)
+print(a, "+", b, "=", a+b)
+
+# 소숫점숫자(상수)의 변수이름은 대문자
+# PI = 3.14
+# PI = 2 BUT, 파이썬은 변수만 허용하기 때문에 아래서 값이 바뀔 수 있음
