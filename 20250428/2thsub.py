dc0b9d43f1e0c1f4e9cefb876b298f214cf0cb9f
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-04-28 17:46:31 +0900

    2일차 과제

---
+++ 20250428/2thsub.py
@@ -0,0 +1,23 @@
+# 파이썬 CodeUp 6012번 문제
+a, b = input(), input()
+a = int(a)
+b = int(b)
+print(a)
+print(b)
+
+# 파이썬 CodeUp 6013번 문제
+a, b = input(), input()
+print(b)
+print(a)
+
+# 파이썬 CodeUp 6014번 문제
+a = input()
+a = float(a)
+print(a)
+print(a)
+print(a)
+
+# 파이썬 CodeUp 6015번 문제
+a, b = input().split()
+print(a)
+print(b)
