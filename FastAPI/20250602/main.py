6660bc6bb8d18a3c7d7910f8227121213cdceeb2
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-06 20:45:09 +0900

    FastAPI3일차 완료

--- FastAPI/20250602/main.py
+++ FastAPI/20250602/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
@@ -18,3 +18,26 @@ class ItemUpdate(BaseModel):
     description: Optional[str] = None
     price: Optional[float] = None
     
+# CRUD 중 Create 기능
+# {}안에 있는건 경로 매개변수
+@app.post("/items/{item_id}", response_model=Item)
+def create_item(item_id:int, item:Item):
+    if item_id in items:
+        # item_id가 items에 있다는것이 참이라면 문제가 있다(예외처리를 해야한다)
+        # raise : 강제로 에러를 터뜨린다 / 강제로 예외를 발생시키다.
+        # status_code : 실패니까 400
+        raise HTTPException(status_code=400, detail="Item already exists")
+    items[item_id] = item
+    return item
+
+# CRUD 중 Read 기능
+# List[Item] : Item만으로 이루어진 List를 반환
+@app.get("/items", response_model=List[Item])
+def read_items():
+    return list(items.values())
+
+@app.get("/items/{item_id}", response_model=Item)
+def read_item(item_id:int):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return items[item_id]
