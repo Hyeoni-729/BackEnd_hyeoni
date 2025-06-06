aa28563017b22f9a8bbe4e469a8447dc4947639a
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-07 00:26:10 +0900

    FastAPI3일차 완료

--- FastAPI/20250602/main.py
+++ FastAPI/20250602/main.py
@@ -8,20 +8,25 @@ app = FastAPI()
 
 items = {}
 
+
 class Item(BaseModel):
     name: str
-    description: Optional[str] = None  # 이 설명에는 빈 값이 들어갈 수도 있다 / 반드시 필요가 없다
+    description: Optional[str] = (
+        None  # 이 설명에는 빈 값이 들어갈 수도 있다 / 반드시 필요가 없다
+    )
     price: float
+
 
 class ItemUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     price: Optional[float] = None
-    
+
+
 # CRUD 중 Create 기능
-# {}안에 있는건 경로 매개변수
+## {}안에 있는건 경로 매개변수
 @app.post("/items/{item_id}", response_model=Item)
-def create_item(item_id:int, item:Item):
+def create_item(item_id: int, item: Item):
     if item_id in items:
         # item_id가 items에 있다는것이 참이라면 문제가 있다(예외처리를 해야한다)
         # raise : 강제로 에러를 터뜨린다 / 강제로 예외를 발생시키다.
@@ -30,19 +35,50 @@ def create_item(item_id:int, item:Item):
     items[item_id] = item
     return item
 
+
 # CRUD 중 Read 기능
-# List[Item] : Item만으로 이루어진 List를 반환
+## List[Item] : Item만으로 이루어진 List를 반환
 @app.get("/items", response_model=List[Item])
 def read_items():
     return list(items.values())
 
-# 키 값들만 반환하기
+
+## 키 값들만 반환하기
 @app.get("/items-keys", response_model=List[int])
 def read_keys():
     return list(items.keys())
 
+
 @app.get("/items/{item_id}", response_model=Item)
-def read_item(item_id:int):
+def read_item(item_id: int):
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
     return items[item_id]
+
+
+# CRUD 중 Update 기능
+@app.put("/items/{item_id}", response_model=Item)
+def update_item(item_id: int, item: ItemUpdate):
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    stored_item: Item = items[item_id]
+    # 변경 전 출력
+    print(
+        f"stored_item name={stored_item.name} price={stored_item.price} description={stored_item.description}"
+    )
+    # update_data = item.dict 에 dict가 밑줄이 쳐지는 이유는 이제 쓰지 않는다. modeol_dump로 대신해서 써라.
+    # class ItemUpdate에서 None필드를 제외한 입력된 것들만 dict타입으로 만들겠다.
+    update_data = item.model_dump(exclude_unset=True)
+
+    print(update_data)
+
+    updated_item = stored_item.model_copy(update=update_data)
+
+    # 변경 후 출력
+    print(
+        f"update_item name={updated_item.name} price={updated_item.price} description={updated_item.description}"
+    )
+
+    items[item_id] = updated_item
+    return updated_item
