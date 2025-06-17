3f87416da55d8848d9a851d34fd4f39cd974a5ff
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-17 12:32:35 +0900

    chat앱 생성 및 등록

---
+++ mydjango/chat/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+# django view : http 요청을 받아 요청을 처리하는 함수
+#  - 항상 1개 인자가 있고, request를 통해 모든 요청 내역을 조회 가능
+def index(request: HttpRequest) -> HttpResponse:
+    # query parameters, form data, files, headers, etc.
+    return HttpResponse("hello django")
