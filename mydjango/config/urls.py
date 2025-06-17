184f2db4071b68162a06499a78e442fefafd33db
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-17 17:54:45 +0900

    mydjango 프로젝트 생성

--- mydjango/config/urls.py
+++ mydjango/config/urls.py
@@ -5,5 +5,6 @@ from django.urls import include  # ADDED
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # chat/urls에 있는 모든 URL 패턴에 일괄적으로 chat/ 이라는 prefix 주소를 부여하겠다.
     path("chat/", include("chat.urls")),  # ADDED
 ]
