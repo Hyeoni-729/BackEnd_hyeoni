a0eadfcb54256884aa85fe2377c4de04e312199b
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-17 12:33:05 +0900

    mydjango 프로젝트 생성

---
+++ mydjango/config/urls.py
@@ -0,0 +1,9 @@
+# config/urls.py 수정
+from django.contrib import admin
+from django.urls import path
+from django.urls import include  # ADDED
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("chat/", include("chat.urls")),  # ADDED
+]
