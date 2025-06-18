d8e99fada6ec7ff5d8fd071f4996168c9fd2fd22
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-18 15:20:43 +0900

    OpenAI로 AI이미지 생성/AI 게임 생성

--- mydjango/chat/urls.py
+++ mydjango/chat/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("messages/new/", views.chat_message_new),
+    # path("puzzle/toy/", views.puzzle_room),
+    # puzzle/ 주소 에 문자열 패턴이 있고, 뒤에 / 가 있으면
+    path("puzzle/<str:name>/", views.puzzle_room),
 ]
--- mydjango/chat/views.py
+++ mydjango/chat/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.http import Http404
 
 # django view : http 요청을 받아 요청을 처리하는 함수
 # django에서는 클래스로 View를 만든다 -> 클래스 기반 뷰
@@ -28,3 +29,36 @@ def chat_message_new(request: HttpRequest) -> HttpResponse:
         answer = "질문 없음"
     
     return HttpResponse(answer)
+
+'''def puzzle_room(request, name):
+    # 없는 데이터는 404 Page not found 응답을 하는 것이 맞다 Django에서는.
+    image_url = {
+        "mario":"static/chat/mario.jpg",
+        "toy":"static/chat/toy-story.jpg",
+    }[name]
+    return render(
+        # 이 템플릿 내의 코드는 모두 그냥 문자열
+        request, template_name="chat/puzzle.html",
+        # "image_url"이라는 이름으로 image_url 값을 전달, 대개 같은 이름으로 지정
+        context={"image_url" : image_url},
+    )'''
+
+# 또는
+
+def puzzle_room(request, name):
+    try:
+        image_url = {
+            "mario":"/static/chat/mario.jpg",
+            "toy":"/static/chat/toy-story.jpg",
+            "openai-1":"/static/chat/openai-1.png"
+        }[name]
+    except KeyError:
+        # 위에서 from django.http import Http404
+        raise Http404(f"not found room : {name}")
+    
+    level = 4
+
+    return render(
+        request, template_name="chat/puzzle.html",
+        context={"image_url" : image_url, "level" : level},
+    )
