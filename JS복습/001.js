0033bf428608d8d3f4b06fbbf4f0b32da4bd0e5e
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-18 18:10:54 +0900

    JS1.3장~2.3장복습(위니북스JS베이스캠프)

---
+++ JS복습/001.js
@@ -0,0 +1,11 @@
+// 라이브 서버 켜서 F12번 눌러서 콘솔 창에서 결과 확인
+let text = 'Hello world JS 복습'
+console.log(text.indexOf('JS', 'python'))
+
+let num = 3;
+console.log(`10 + 3 = ${10 + 3}`); // 10 + 3 = 13
+console.log(`증감연산 : ${++num}`); // 증감연산 : 4
+console.log((3).toString(2)); // 11
+
+let a;
+console.log(a); // undefined
