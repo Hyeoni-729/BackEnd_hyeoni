4c23ffece8825f86b109d143bd2d8a29fd1e8a0b
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-20 14:47:46 +0900

    blog.Post Admin 옵션 커스터마이징

--- mydjango/blog/admin.py
+++ mydjango/blog/admin.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
 from . models import Post
 
-# admin.site.register(Post) -> 첫 번째 방법
-
-# # 두 번째 방법
-# class PostAdmin(admin.ModelAdmin):
-#     pass
-
-# # PostAdmin 룰에 맞춰 admin에 등록해라
-# admin.site.register(Post, PostAdmin)
-
-# 세 번째 방법
 @admin.register(Post) # -> python의 장식자 문법
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "status"]
+    search_fields = ["title"]
+    list_filter = ["status"]
--- mydjango/make_blog_posts.py
+++ mydjango/make_blog_posts.py
@@ -1,13 +1,15 @@
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 import django
+
 django.setup()
 
 from pyhub.llm import OpenAILLM, UpstageLLM
 from django.conf import settings
 from pydantic import BaseModel  # 추가
-from blog.models import Post 
+from blog.models import Post
 
 
 class BlogPost(BaseModel):
@@ -18,17 +20,25 @@ class BlogPost(BaseModel):
 llm = OpenAILLM(
     api_key=settings.OPENAI_API_KEY,
     model="gpt-4o-mini",
-    system_prompt="""니는 여행 블로거다. 유저가 제시하는 내용에 맞춰서 블로그의 제목과 내용 작성해줘""", max_tokens=8000,
+    system_prompt="""니는 여행 블로거다. 유저가 제시하는 내용에 맞춰서 블로그의 제목과 내용 작성해줘""",
+    max_tokens=8000,
 )
-reply = llm.ask("울산 고래 버스", schema=BlogPost)
 
-blog_post: BlogPost = reply.structured_data
-blog_post.title
-blog_post.content
+# 키워드로 블로그 포스팅
+keywords = ["부산 돼지국밥", "붕어빵",]
 
-post = Post()
-post.title = blog_post.title
-post.content = blog_post.content
-post.save()
+for keyword in keywords:
+    reply = llm.ask(
+        keyword,
+        schema=BlogPost,
+        use_history=False,
+    )
+    print(reply)
+    blog_post: BlogPost = reply.structured_data
 
-print("saved post #", post.id)
+    post = Post()
+    post.title = blog_post.title
+    post.content = blog_post.content
+    post.save()
+
+    print("saved post #", post.id)
