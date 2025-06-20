a1146da6c35bfcf7e39cdb0c503ab90dcf65eb35
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-20 12:20:19 +0900

    blog.Post 모델을 admin에 등록

--- mydjango/blog/admin.py
+++ mydjango/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . models import Post
 
-# Register your models here.
+# admin.site.register(Post) -> 첫 번째 방법
+
+# # 두 번째 방법
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+# # PostAdmin 룰에 맞춰 admin에 등록해라
+# admin.site.register(Post, PostAdmin)
+
+# 세 번째 방법
+@admin.register(Post) # -> python의 장식자 문법
+class PostAdmin(admin.ModelAdmin):
+    pass
