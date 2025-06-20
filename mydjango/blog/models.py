df8da3d688a02a01c963ebc122a46be3599d4276
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-20 12:02:04 +0900

    blog.Post 모델 정의 및 migration 파일 자동생성

--- mydjango/blog/models.py
+++ mydjango/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ("draft", "임시"),
+        ("published", "공개"),
+        ("private", "비공개"),
+    ]
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
