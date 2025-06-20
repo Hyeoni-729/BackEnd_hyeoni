c6821df808fe96f04eeaca4e821c2b18144e6001
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-20 16:22:51 +0900

    comment 모델 생성

--- mydjango/blog/urls.py
+++ mydjango/blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 # 장고의 urls.py한테 장고가 요구하는 것은 단 하나.
-# urlpatterns 이름의 리스트 => 이름에 오타가 있으면 절대 안된다.
-urlpatterns = []
+# urlpatterns 이름의 리스트 => 이름에 오타가 있으면 절대 안 된다.
+urlpatterns = [
+    path("", views.post_List),
+    path("<int:pk>/", views.post_detail),
+]
--- mydjango/blog/views.py
+++ mydjango/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+# 목록 조회
+post_List = ListView.as_view(
+    model=Post,
+    # 생략하면 "앱이름/모델명소문자_list.html"을 자동으로 찾아준다
+    # template_name="blog/post_list.html"
+)
+
+# 단건 조회
+post_detail = DetailView.as_view(
+    model=Post,
+)
+
+# def post_detail(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(request, "blog/post_detail.html", {"post":post})
