c64863443baf15328d2287b614e0c3be8d09ddc8
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-25 08:47:36 +0900

    baemin 페이지에 리뷰 목록 보여주기

--- mydjango/baemin/urls.py
+++ mydjango/baemin/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    
+    path("", views.shop_list),
+    path("<int:pk>/", views.shop_detail),
+    path("<int:shop_pk>/reviews/new/", views.review_new),
 ]
--- mydjango/baemin/views.py
+++ mydjango/baemin/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Shop, Review
+from .forms import ReviewForm
+from django.contrib import messages
 
-# Create your views here.
+
+def shop_list(request):
+    # DB에서 baemin_shop 테이블의 모든 레코드를 조회할 준비
+    qs = Shop.objects.all()
+
+    return render(
+        request,
+        template_name="baemin/shop_list.html",
+        context={
+            "shop_list": qs,
+        },
+    )
+
+
+def shop_detail(request, pk):
+    shop = Shop.objects.get(pk=pk)
+
+    # 전체(모든 Shop) 리뷰 데이터를 가져올 준비.
+    review_qs = Review.objects.all()
+    # 특정 shop의 리뷰 데이터를 가져올 준비 (가져올 범위가 좁혀집니다.)
+    review_qs = review_qs.filter(shop=shop)
+
+    return render(
+        request,
+        template_name="baemin/shop_detail.html",
+        context={"shop": shop, "review_list": review_qs},
+    )
+
+
+def review_new(request, shop_pk):
+    shop = get_object_or_404(Shop, pk=shop_pk)
+
+    if request.method == "GET":
+        form = ReviewForm()
+
+    else:
+        form = ReviewForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.shop = shop
+            review.save()
+            messages.success(request, "댓글이 성공적으로 등록되었습니다!")
+            shop_url = f"/baemin/{shop.pk}/"
+            return redirect(shop_url)
+
+    return render(
+        request,
+        template_name="baemin/review_form.html",
+        context={"form": form, "shop": shop},
+    )
