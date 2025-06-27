205f9c6c0d71a2329ce6013275eefa72decd6105
Hyeoni-729 <bboongbbaang12@gmail.com>
2025-06-27 16:22:44 +0900

    auth.UserCreationForm 을 활용한 회원가입 화면 구현

--- mydjango/accounts/views.py
+++ mydjango/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def signup(request):
+    if request.method == "GET":
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save() # 회원가입 완료. 지정 유저 레코드 DB에 저장됨
+            # 회원가입을 했으니 로그인 페이지로 이동하는 것이 자연스럽다.
+            return redirect("/accounts/login/") # 이 페이지는 아직 없다.
+        
+
+    return render(request, "accounts/signup_form.html", {"form":form},)
